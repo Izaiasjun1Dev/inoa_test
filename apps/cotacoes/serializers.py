fc2b7ea97b9afd7d657d018c3d98f7825c5bb319
Izaiasjun1Dev <solucaoprogramer@gmail.com>
2021-09-20 16:22:23 -0300

    ajustes nos serializers de user e cotacoes

--- apps/cotacoes/serializers.py
+++ apps/cotacoes/serializers.py
@@ -1,3 +1,4 @@
+from apps.user.serializers import UserSerializer
 from rest_framework import serializers
 from .models import Cotacao, Wallet
 
@@ -7,6 +8,7 @@ class CotacaoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class WalletSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Wallet
         fields = '__all__'
